53ffdccc25709db83cbe46a31aeff01787ac22d6
Ojas Aklecha <ojasaklechayt@gmail.com>
2023-05-29 20:38:37 +0530

    Updated the directory

--- src/components/Button/Rounded Button/RoundButton.stories.js
+++ src/components/Button/Rounded Button/RoundButton.stories.js
@@ -2,7 +2,7 @@ import  Button  from './RoundButton';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
-  title: 'Example/RoundButtons',
+  title: 'Example/Buttons/Round \Buttons',
   component: Button,
   tags: ['autodocs'],
   argTypes: { size: { control: 'radio', options: ['small', 'medium', 'large'] },
--- src/components/Button/Square Button/Button.stories.js
+++ src/components/Button/Square Button/Button.stories.js
@@ -2,7 +2,7 @@ import  Button  from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
-  title: 'Example/Buttons',
+  title: 'Example/Buttons/Square \ Buttons',
   component: Button,
   tags: ['autodocs'],
   argTypes: { size: { control: 'radio', options: ['small', 'medium', 'large'] },
