4662ce68ed723d020a71a6b0ec614a7c58d425c9
Nishita Pande <pande.nishita@gmail.com>
2023-06-15 19:13:57 +0530

    Revert "added styles to the component"
    This reverts commit eb7fd1508ddba85374037156da0e6c05f29af834.

--- src/components/product-display/ProductDetail.jsx
+++ src/components/product-display/ProductDetail.jsx
@@ -1,49 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import "./ProductDetail.scss";
-import instock from "./images/in-stock.png";
 
-const ProductDetail = ({
-  isDarkMode,
-  heading,
-  reviews,
-  inStock,
-  price,
-  detail1,
-  detail2,
-  detail3,
-  detail4,
-  detail5,
-  text1,
-  text2,
-  text3,
-  text4,
-  text5,
-  type1,
-  type2,
-  type3,
-  type4,
-  descriptionText,
-}) => {
+const ProductDetail = ({ isDarkMode, heading }) => {
   const productDetail = `productdetail ${
     isDarkMode ? "dark-mode" : "light-mode"
   }`;
-  const [count, setCount] = useState(0);
-
-  const handleIncrement = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
-
-  const handleDecrement = () => {
-    if (count > 0) {
-      setCount((prevCount) => prevCount - 1);
-    }
-  };
-  const [selectedOption, setSelectedOption] = useState("");
-
-  const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value);
-  };
   return (
     <div className={productDetail}>
       <div className="">
@@ -51,106 +12,7 @@ const ProductDetail = ({
         <div className="right-box">
           <div className="text-box">
             <div className="content">
-              <h1 className="primary-heading"> {heading} </h1>
-              <div className="review">
-                <div className="stars">
-                  <span className="star">
-                    &#9733;&#9733;&#9733;&#9733;&#9733;
-                  </span>
-                  <p className="review-text">({reviews})</p>
-                </div>
-                <div className="stock">
-                  <img src={instock} className="instock-img" />
-                  <p className="instock-text">{inStock}</p>
-                </div>
-              </div>
-              <div className="price-box">
-                <h2 className="secondary-heading"> {price} </h2>
-                <p className="price-text"> Includes all taxes</p>
-              </div>
-              <p className="description-text"> {descriptionText} </p>
-              <div className="product-details-box">
-                <div className="product-detail1">
-                  <p className="product-detail"> {detail1} </p>
-                  <p className="detail-text"> {text1} </p>
-                </div>
-                <div className="product-detail2">
-                  <p className="product-detail"> {detail2} </p>
-                  <p className="detail-text"> {text2} </p>
-                </div>
-                <div className="product-detail3">
-                  <p className="product-detail"> {detail3} </p>
-                  <p className="detail-text"> {text3} </p>
-                </div>
-                <div className="product-detail4">
-                  <p className="product-detail"> {detail4} </p>
-                  <p className="detail-text"> {text4} </p>
-                </div>
-                <div className="product-detail5">
-                  <p className="product-detail"> {detail5} </p>
-                  <p className="detail-text"> {text5} </p>
-                </div>
-              </div>
-              <div className="quantity-and-size-box">
-                <div className="quantity-box">
-                  <button onClick={handleDecrement} className="quantity-button">
-                    {" "}
-                    -{" "}
-                  </button>
-                  <p className="quantity-text"> {count}</p>
-                  <button onClick={handleIncrement} className="quantity-button">
-                    {" "}
-                    +{" "}
-                  </button>
-                </div>
-                <div className="size-box">
-                  {" "}
-                  <label>
-                    <input
-                      type="radio"
-                      value="option1"
-                      checked={selectedOption === "option1"}
-                      onChange={handleOptionChange}
-                    />
-                    {type1}
-                  </label>
-                  <label>
-                    <input
-                      type="radio"
-                      value="option2"
-                      checked={selectedOption === "option2"}
-                      onChange={handleOptionChange}
-                    />
-                    {type2}
-                  </label>
-                  <label>
-                    <input
-                      type="radio"
-                      value="option3"
-                      checked={selectedOption === "option3"}
-                      onChange={handleOptionChange}
-                    />
-                    {type3}
-                  </label>
-                  <label>
-                    <input
-                      type="radio"
-                      value="option4"
-                      checked={selectedOption === "option4"}
-                      onChange={handleOptionChange}
-                    />
-                    {type4}
-                  </label>
-                </div>
-              </div>
-              <div className="button-box">
-                <a href="#" className="buy-now-button">
-                  Buy Now
-                </a>
-                <a href="#" className="addto-cart-button">
-                  Add to Cart
-                </a>
-              </div>
+              <h1> {heading} </h1>
             </div>
           </div>
         </div>
@@ -161,25 +23,6 @@ const ProductDetail = ({
 
 ProductDetail.propTypes = {
   heading: PropTypes.string.isRequired,
-  reviews: PropTypes.string.isRequired,
-  inStock: PropTypes.string.isRequired,
-  price: PropTypes.string.isRequired,
-  detail1: PropTypes.string.isRequired,
-  detail2: PropTypes.string.isRequired,
-  detail3: PropTypes.string.isRequired,
-  detail4: PropTypes.string.isRequired,
-  detail5: PropTypes.string.isRequired,
-  text1: PropTypes.string.isRequired,
-  text2: PropTypes.string.isRequired,
-  text3: PropTypes.string.isRequired,
-  text4: PropTypes.string.isRequired,
-  text5: PropTypes.string.isRequired,
-  type1: PropTypes.string.isRequired,
-  type2: PropTypes.string.isRequired,
-  type3: PropTypes.string.isRequired,
-  type4: PropTypes.string.isRequired,
-  descriptionText: PropTypes.string.isRequired,
   isDarkMode: PropTypes.bool,
 };
-
 export default ProductDetail;
--- src/components/product-display/ProductDetail.stories.js
+++ src/components/product-display/ProductDetail.stories.js
@@ -12,27 +12,7 @@ const Template = (args) => <ProductDetail {...args} />;
 export const Default = Template.bind({});
 
 Default.args = {
-  heading: "Nike Mens Precision Vi Running Shoe ",
-  reviews: "4.3k reviews",
-  inStock: "In Stock",
-  price: "$69.18",
-  descriptionText:
-    "Step up your game with NIKE shoes. Engineered for performance and style, these shoes deliver unparalleled comfort, support, and durability. Elevate your stride, unleash your potential.",
-  detail1: "Sole",
-  detail2: "Closure",
-  detail3: "Fit Type",
-  detail4: "Shoe Width",
-  detail5: "Shoe Type",
-  text1: "Rubber",
-  text2: "Lace-Up",
-  text3: "Regular",
-  text4: "Medium",
-  text5: "NIKE PRECISION VI MEN'S BASKETBALL SHOES ",
-  type1: "4.5 UK (5 US)",
-  type2: "6 UK (7US)",
-  type3: "8 UK (9 US)",
-  type4: "10 UK (11 US)",
-
+  heading: "Home",
   isDarkMode: true,
   isArrow: false,
   isStyle1: true,
